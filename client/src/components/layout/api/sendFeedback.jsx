739de0cc8ee63b71024d60f625bcd922433e199e
Gleb <filingleb06@gmail.com>
2025-06-19 10:22:44 +0200

    feedback fix

--- client/src/components/layout/api/sendFeedback.jsx
+++ client/src/components/layout/api/sendFeedback.jsx
@@ -2,7 +2,7 @@ const SendFeedback = async ({ problem , name }) => {
 
     const apiUrl = process.env.REACT_APP_API_URL
     try{
-        const response = await fetch(`h${apiUrl}/api/feedback` , {
+        const response = await fetch(`${apiUrl}/api/feedback` , {
             method: "POST",
             headers: {'Content-Type': 'application/json' , 'Accept': 'application/json'},
             body: JSON.stringify({problem: problem , name: name})
