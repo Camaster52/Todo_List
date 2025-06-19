b38cfe27dd45f48291627ec9b2dfe72fb4a4988d
Gleb <filingleb06@gmail.com>
2025-06-19 11:51:30 +0200

    deploy fix 2.0

--- client/src/components/layout/api/sendFeedback.jsx
+++ client/src/components/layout/api/sendFeedback.jsx
@@ -7,13 +7,14 @@ const SendFeedback = async ({ problem , name }) => {
             body: JSON.stringify({problem: problem , name: name})
         })
 
+        const result = await response.json()
+
         if (!response.ok) {
-            const errorData = await response.json();
-            throw new Error(errorData.message || "Server error");
+            throw new Error(result.message || "Server error")
         }
 
-        console.log("Success feedback: " , result)
-        return await response.json();
+        console.log("Success feedback:", result)
+        return result
     }catch(error){
         console.error("Error: ", error.message)
         throw error
